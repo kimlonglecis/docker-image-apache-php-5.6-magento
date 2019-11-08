db2bcaa18ad222347898e8f1f4823de87e28dc61
Anthony Rusdi <33247310+antrusd@users.noreply.github.com>
2019-11-09 01:05:36 +0700

    lib/Zend/Mail/Protocol/Smtp.php
    * hardcode ehlo and helo domain
    * docker run must have env EHLO_HELO_DOMAIN configured
    
    Signed-off-by: Anthony Rusdi <33247310+antrusd@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ -f /var/www/html/lib/Zend/Mail/Protocol/Smtp.php ]; then
+    sed -i "s/_send('EHLO .*$/_send('HELO $EHLO_HELO_DOMAIN')\;/" /var/www/html/lib/Zend/Mail/Protocol/Smtp.php
+    sed -i "s/_send('HELO .*$/_send('HELO $EHLO_HELO_DOMAIN')\;/" /var/www/html/lib/Zend/Mail/Protocol/Smtp.php
+fi
+
 # Start the ssh server
 /etc/init.d/ssh start && /sbin/my_init
 
